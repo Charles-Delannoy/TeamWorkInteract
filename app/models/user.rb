a366062438ba6b5401886b2e63ae947e236632c6
charles delannoy <charles.delannoy@hei.fr>
2021-02-02 12:53:33 +0100

    add validation and default A for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :user_type, inclusion: {in: %w(A CP M), message: 'doit être Admin, CP ou membre'}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
