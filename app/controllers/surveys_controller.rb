713d5a05fb41de76c8ac38cf08a1b84d668457aa
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-02-02 08:16:37 +0100

    survey sans condition first_survey?

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,22 +2,18 @@ class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy]
 
   def new
-    if first_survey?
-      @survey = Survey.new
-      authorize @survey
-    end
+    @survey = Survey.new
+    authorize @survey
   end
 
   def create
-    if first_survey?
-      @survey =Survey.new(surveys_params)
-      @survey.user = current_user
-      authorize @survey
-      if @survey.save
-        redirect_to survey_path(@survey)
-      else
-        render :new
-      end
+    @survey =Survey.new(surveys_params)
+    @survey.user = current_user
+    authorize @survey
+    if @survey.save
+      redirect_to survey_path(@survey)
+    else
+      render :new
     end
   end
 
@@ -25,21 +21,6 @@ class SurveysController < ApplicationController
   end
 
   private
-  
-  def first_survey?
-    surveys = Survey.all
-    unless surveys.empty?
-      surveys.each do |survey|
-        if survey.user == current_user
-          return false
-        else
-          return true
-        end
-      end
-    else
-      true
-    end
-  end
 
   def set_survey
     @survey = Survey.find(params[:id])
