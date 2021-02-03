f6e3237cbe437230de439ef75c4e57c97d70d0f4
charles delannoy <charles.delannoy@hei.fr>
2021-02-03 09:15:08 +0100

    correction typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :user_type, inclusion: {in: %w(A R M), message: 'doit être Admin, Réfétent ou membre'}
+  validates :user_type, inclusion: {in: %w(A R M), message: 'doit être Admin, Référent ou membre'}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
