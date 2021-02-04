6ce9c9a28218221cffa781274ee1f3e3b33b28b1
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-02-04 14:57:09 +0100

    controller ok. before policy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,15 @@
 class QuestionsController < ApplicationController
 
   def new
+    @survey = Survey.find(params[:survey_id])
     @question = Question.new
     authorize @question
   end
 
   def create
     @question = Question.new(questions_params)
+    @survey = Survey.find(params[:survey_id])
+    @question.survey = @survey
     @question.user = current_user
     authorize @question
     if @question.save
@@ -19,6 +22,6 @@ class QuestionsController < ApplicationController
   private
 
   def questions_params
-    params.require(:question).permit(:title, :coef)
+    params.require(:question).permit(:title, :coef, :axe_id)
   end
 end
