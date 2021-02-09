c55423052db8b6639eb80ce580382d696e16688f
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-02-09 14:19:27 +0100

    before change

--- db/migrate/20210209131532_create_group_campaigns.rb
+++ db/migrate/20210209131532_create_group_campaigns.rb
@@ -3,6 +3,8 @@ class CreateGroupCampaigns < ActiveRecord::Migration[6.0]
     create_table :group_campaigns do |t|
       t.references :group, null: false, foreign_key: true
       t.references :campaign, null: false, foreign_key: true
+
+      t.timestamps
     end
   end
 end
