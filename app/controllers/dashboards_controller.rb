0d808ce259004dc8ddc6dcdb4c2f7d1216010121
charles delannoy <charles.delannoy@hei.fr>
2021-02-17 00:38:41 +0100

    dasboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,13 @@ class DashboardsController < ApplicationController
   skip_after_action :verify_authorized
 
   def show
-    @groups = current_user.groups unless current_user.admin
+    @groups = current_user.groups unless admin?
+
+  end
+
+  private
+
+  def admin?
+    current_user.admin == 'Y'
   end
 end
