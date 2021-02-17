996c402c34f1babb8506a5fba215ae6ff84f6133
charles delannoy <charles.delannoy@hei.fr>
2021-02-17 01:05:30 +0100

    routes & controllers architecture review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,19 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    redirect_to dashboard_path if current_user && (current_user.first_name && current_user.last_name)
-    redirect_to validation_path if current_user && !(current_user.first_name && current_user.last_name)
-  end
-
-  def validate_account
-    current_user.last_name = user_params[:last_name]
-    current_user.first_name = user_params[:first_name]
-    current_user.save ? redirect_to(root_path) : (render :new)
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    if current_user
+      redirect_to edit_validate_accounts_path if current_user.first_name.nil? && current_user.last_name.nil?
+    else
+      redirect_to dashboard_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :admin_users, only: [:index, :create]
   root to: 'pages#home'
+
+  devise_for :users
+
+  resources :admin_users, only: [:index, :create]
+
   resource :dashboard, only: :show
-  get 'validation', to: 'pages#validate_account_edit'
-  patch 'validation', to: 'pages#validate_account'
+
+  resource :validate_accounts, only: [:edit, :update]
 
   resources :surveys, only: [:index, :new, :create, :destroy, :edit, :update, :show] do
     resources :questions, only: [:new, :create ]
   end
+
   resources :questions, only: [ :show, :edit, :update, :destroy ] do
     resources :propositions, only: [:create]
   end
+
   resources :propositions, only: [ :edit, :update, :destroy ]
 
   resources :groups, only: [:index, :new, :create, :destroy, :edit, :update]
@@ -19,6 +24,7 @@ Rails.application.routes.draw do
   resources :axes, only: [:index, :new, :create, :destroy, :edit, :update, :show] do
     resources :recommandations, only: [:new, :create]
   end
+
   resources :recommandations, only: [:edit, :update, :destroy]
 
   resources :campaigns, only: [:index, :show, :new, :create, :edit, :update, :destroy]
