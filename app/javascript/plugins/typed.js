6ee9be626e0e4a6061e51c78f143bbeec69d8abd
charles delannoy <charles.delannoy@hei.fr>
2021-02-21 13:30:31 +0100

    home page design

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -4,7 +4,7 @@ const typed = () => {
   const homeTitle = document.getElementById('home-typed-text');
   if (homeTitle) {
     new Typed('#home-typed-text', {
-      strings: ["TeamWorkInteract", "Suivez les équipes", "Améliorer leur cohésion"],
+      strings: ["Créez vos questionnaires", "Suivez vos équipes", "Améliorer leur cohésion"],
       typeSpeed: 120,
       loop: true
     });
