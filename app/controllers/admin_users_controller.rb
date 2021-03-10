0e1bb73a51fedb651d49d539b21eacb516048edd
charles delannoy <charles.delannoy@hei.fr>
2021-03-10 17:14:58 +0100

    url fix

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -37,9 +37,10 @@ class AdminUsersController < ApplicationController
       @user.reset_password_token = hashed
       @user.reset_password_sent_at = Time.now.utc
       @user.save
+      http = Rails.env.development? ? '' : "https://"
       port = Rails.env.development? ? ':3000' : ""
       base_url = ActionMailer::Base.default_url_options[:host]
-      reset_pwd_url = "#{base_url}#{port}/users/password/edit?reset_password_token=#{raw}"
+      reset_pwd_url = "#{http}#{base_url}#{port}/users/password/edit?reset_password_token=#{raw}"
 
       UserMailer.first_welcome(@user, @group.name, @role, reset_pwd_url).deliver_later
     else
