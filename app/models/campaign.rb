42e6f75ccef18260bad35206d18deae1074e4ea3
charles delannoy <charles.delannoy@hei.fr>
2021-03-12 17:35:54 +0100

    seed and some dependent destroy

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ApplicationRecord
   belongs_to :survey
-  has_many :group_campaigns
+  has_many :group_campaigns, dependent: :destroy
   has_many :groups, through: :group_campaigns
 end
