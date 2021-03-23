315ed5a85dc4a32fe4bbb1c2b62ff7d7cea5db36
charles delannoy <charles.delannoy@hei.fr>
2021-03-23 14:54:42 +0100

    campaign model tests with rspec

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,15 @@ class Campaign < ApplicationRecord
   belongs_to :survey
   has_many :group_campaigns, dependent: :destroy
   has_many :groups, through: :group_campaigns
+
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_before_end
+
+  private
+
+  def start_before_end
+    errors.add :end_date, "must be after starts_at" if start_date && end_date && end_date <= start_date
+  end
 end
---
+++ test/factories/campaigns.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :campaign do
+    title { "My campaign" }
+    start_date { Date.today }
+    end_date { Date.today + 30 }
+
+    factory :campaign_no_title do
+      title { nil }
+    end
+
+    factory :campaign_no_start do
+      start_date { nil }
+    end
+
+    factory :campaign_no_end do
+      end_date { nil }
+    end
+
+    factory :campaign_start_after_en do
+      start_date { Date.today + 40 }
+    end
+  end
+end
