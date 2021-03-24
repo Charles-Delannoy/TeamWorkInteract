8a6510d8548412107e0502fc30c14286f452d83c
charles delannoy <charles.delannoy@hei.fr>
2021-03-24 16:26:24 +0100

    send first welcome email with job

---
+++ app/jobs/send_first_welcome_mail_job.rb
@@ -0,0 +1,16 @@
+class SendFirstWelcomeMailJob < ApplicationJob
+  queue_as :mailer
+
+  def perform(user, group, role)
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    user.reset_password_token = hashed
+    user.reset_password_sent_at = Time.now.utc
+    user.save
+    http = Rails.env.development? ? '' : "https://"
+    port = Rails.env.development? ? ':3000' : ""
+    base_url = ActionMailer::Base.default_url_options[:host]
+    reset_pwd_url = "#{http}#{base_url}#{port}/users/password/edit?reset_password_token=#{raw}"
+
+    UserMailer.first_welcome(user, group.name, role, reset_pwd_url).deliver_later
+  end
+end
