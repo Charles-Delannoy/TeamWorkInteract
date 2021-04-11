81289b76f150537a1d057b747441ab4a9648ff14
charles delannoy <charles.delannoy@hei.fr>
2021-04-11 19:20:23 +0200

    axe validation test

--- spec/factories/axes.rb
+++ spec/factories/axes.rb
@@ -2,5 +2,17 @@ FactoryBot.define do
   factory :axe do
     sequence(:title) { |i| "Title #{i}" }
     sequence(:description) { |i| "Ma super description #{i}" }
+    factory :axe_no_title do
+      title { nil }
+    end
+    factory :axe_short_title do
+      title { 'ax' }
+    end
+    factory :axe_no_description do
+      description { nil }
+    end
+    factory :axe_short_description do
+      description { 'un axe' }
+    end
   end
 end
