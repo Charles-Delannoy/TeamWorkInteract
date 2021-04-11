4a19deb7889bdeb7070beeda64da117a90bf3d79
charles delannoy <charles.delannoy@hei.fr>
2021-04-11 21:04:31 +0200

    flatpickr fix

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,12 +2,14 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpicker = () => {
-  let calendar = flatpickr("#range_start", {
-    altInput: true,
-    inline: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
-  });
-  calendar.clear()
+  if (document.getElementById("range_start")) {
+    let calendar = flatpickr("#range_start", {
+      altInput: true,
+      inline: true,
+      plugins: [new rangePlugin({ input: "#range_end"})]
+    });
+    calendar.clear();
+  }
 }
 
 export { initFlatpicker };
