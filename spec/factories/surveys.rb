43cbc4cf4aef6bca973c4da518b44c440556c7f7
charles delannoy <charles.delannoy@hei.fr>
2021-04-11 20:57:18 +0200

    survey validations

--- spec/factories/surveys.rb
+++ spec/factories/surveys.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :survey do
     title {"Mon questionnaire"}
     description {"Descritpion de mon questionnaire"}
+
+    factory :survey_no_title do
+      title {""}
+    end
+
+    factory :survey_no_description do
+      description {""}
+    end
   end
 end
