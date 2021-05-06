80d727f8fe99ce059205e5c95a698e6ed9c8ede8
charles delannoy <charles.delannoy@hei.fr>
2021-05-06 19:53:14 +0200

    previous and back btn

--- app/javascript/controllers/survey-navigation_controller.js
+++ app/javascript/controllers/survey-navigation_controller.js
@@ -11,4 +11,24 @@ export default class extends Controller {
     nextQuestion.classList.remove('hidden-question');
     e.currentTarget.classList.add('active');
   }
+
+  next = (e) => {
+    e.preventDefault();
+    const nextTag = document.querySelector('.question-access.active').nextElementSibling;
+    nextTag.click();
+    if ((nextTag.nextElementSibling === null)) {
+      document.querySelector('.next').classList.add('hidden-button');
+    }
+    document.querySelector('.back').classList.remove('hidden-button');
+  }
+
+  previous = (e) => {
+    e.preventDefault();
+    const prevTag = document.querySelector('.question-access.active').previousElementSibling;
+    prevTag.click();
+    if (!(prevTag.previousElementSibling.classList[0] === "question-access")) {
+      document.querySelector('.back').classList.add('hidden-button');
+    }
+    document.querySelector('.next').classList.remove('hidden-button');
+  }
 }
