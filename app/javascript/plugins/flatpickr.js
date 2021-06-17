d956ad4db827ac8b70e537ee1bc4493105dbc0ca
charles delannoy <charles.delannoy@hei.fr>
2021-06-17 15:42:55 +0200

    fix delete animation

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,18 +3,17 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpicker = () => {
   if (document.getElementById("range_start") || document.getElementById("range_start_edit")) {
-
     let calendar = flatpickr("#range_start", {
       altInput: true,
       inline: true,
       plugins: [new rangePlugin({ input: "#range_end"})]
     });
     if (document.getElementById("range_start")) {
+      console.log('COUCOU')
       calendar.clear();
     }
 
   } else if (document.getElementById("range_start_edit")) {
-
     let calendar_edit = flatpickr("#range_start_edit", {
       altInput: true,
       inline: true,
