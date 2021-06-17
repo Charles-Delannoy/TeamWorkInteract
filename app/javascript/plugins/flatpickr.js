47e8b845c4376c58c124d22aa98c6b7330dd90bc
charles delannoy <charles.delannoy@hei.fr>
2021-06-17 16:03:38 +0200

    flatpickr fix for edit

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,7 +2,7 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpicker = () => {
-  if (document.getElementById("range_start") || document.getElementById("range_start_edit")) {
+  if (document.getElementById("range_start")) {
     let calendar = flatpickr("#range_start", {
       altInput: true,
       inline: true,
